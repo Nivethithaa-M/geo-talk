912d1f1d9f80a6f4fc81d2b9c4a701d047f0901f
student <student@TFPC69.ffdc.audist>
2021-12-16 16:39:07 +0530

    map changes :sparkles:

--- screens/sidebar/Map.js
+++ screens/sidebar/Map.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import { StyleSheet,TextInput,TouchableOpacity, View, Dimensions } from "react-native";
+import MapView, { Marker, Circle } from "react-native-maps";
 import * as Location from 'expo-location';
-import { RFC_2822 } from "moment";
+import { FontAwesome5,MaterialIcons } from "@expo/vector-icons";
 
 const { width, height } = Dimensions.get("screen");
+const LATDELTA = 0.017
+const LNGDELTA = 0.017
 
 const Map = (props) => {
   const [location, setLocation] = useState(null);
@@ -13,9 +15,48 @@ const Map = (props) => {
   const [mapRegion, setMapRegion] = useState({
     longitude: 80.23539830006482,
     latitude: 13.01107648890877,
-    longitudeDelta: 0.8023539830006482,
-    latitudeDelta: 0.1301107648890877
+    longitudeDelta: LNGDELTA,
+    latitudeDelta: LATDELTA
   });
+  const [friends] = useState([
+    {
+      title: "bob",
+      messsage: "guindy park",
+      icon: "dog",
+      location: {
+        longitude: 80.2378081241749,
+        latitude: 13.005155071011641
+      }
+    },
+    {
+      title: "Alex",
+      messsage: "Childhood friend",
+      icon: "dragon",
+      location: {
+        longitude: 80.2381648579569,
+        latitude: 13.007708359268626
+      }
+    },
+    {
+      title: "Jack",
+      messsage: "Business Partner",
+      icon: "dove",
+      location: {
+        longitude: 80.22799707958029,
+        latitude: 13.015214374137933, 
+      }
+    },
+    {
+      title: "rose",
+      messsage: "coffee",
+      icon: "cat",
+      location: {
+        longitude: 80.23329712442346,
+        latitude: 13.013510494203762,  
+      }
+    }
+    
+  ]);
   useEffect(() => {
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -25,29 +66,62 @@ const Map = (props) => {
       }
 
       let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
       setMapRegion({
         longitude: location.coords.longitude,
         latitude: location.coords.latitude,
-        longitudeDelta: 0.8023539830006482,
-        latitudeDelta: 0.1301107648890877
+        longitudeDelta: LNGDELTA,
+        latitudeDelta: LATDELTA
       });
     })();
   }, []);
 
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" />      
       <MapView
         initialRegion={mapRegion}
         style={styles.mapView}
       >
+        {mapRegion ? (
+          <Circle
+            center={{
+              longitude: mapRegion.longitude,
+              latitude: mapRegion.latitude
+            }}
+            radius={1000}
+            strokeColor="transparent"
+            fillColor="rgba(0,255,0,0.1)"
+          ></Circle>
+        ) : null}
+        {mapRegion ? (
+          <Marker
+            coordinate={{
+              longitude: mapRegion.longitude,
+              latitude: mapRegion.latitude
+            }}
+            title="Me"
+            description="Myself"
+          >
+          </Marker>
+        ) : null}
+
+        {friends ? friends.map((friend, i) => (
+          <Marker key={i} coordinate={{
+            ...friend.location,  longitudeDelta: LNGDELTA,
+            latitudeDelta: LATDELTA
+          }} title={friend.title} description={friend.messsage}>
+            <FontAwesome5 name={friend.icon} size={26} />
+          </Marker>
+        )) : null}
       </MapView>
-      <Marker coordinate={mapRegion}>
-        <View style={styles.circle}>
-          <View style={styles.stroke} />
-          <View style={styles.core} />
+      <View style={styles.inputWrapper}>
+          <TextInput
+            style={styles.input}
+            placeholder="Type your message here"
+          />
+            <MaterialIcons style={styles.sendButton} name="send" size={32} color="#fe4027" />
         </View>
-      </Marker>
     </View>
   );
 };
@@ -56,7 +130,8 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     width,
-    height
+    height,
+    flex:1 
   },
   mapView: {
     position: "absolute",
@@ -65,7 +140,8 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     width,
-    height
+    height,
+    flex:1 
   },
   circle: {
     width: 26,
@@ -96,6 +172,36 @@ const styles = StyleSheet.create({
     height: 24,
     borderRadius: 50,
     zIndex: 2
+  },
+  inputWrapper: {
+    width: '100%',
+    position: 'absolute',
+    padding: 10,
+    // top: StatusBar.currentHeight,
+    bottom: 0,
+    left: 0,
+    zIndex: 100,
+    flexDirection:'row'
+  },
+  input: {
+    height: 46,
+    width: '100%',
+    paddingVertical: 10,
+    paddingRight: 50,
+    paddingLeft: 10,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 6,
+    borderColor: '#ccc',
+    backgroundColor: '#fff'
+  },
+  sendButton: {
+    position: 'absolute',
+    top: 17,
+    right:20
+  },
+  sendButtonActive: {
+    opacity: 1
   }
 });
 
--- screens/sidebar/Profile.js
+++ screens/sidebar/Profile.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { View, Text } from "react-native";
-
+import { auth } from '../../firebase'
 export default function Profile() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text style={{fontSize:16,fontWeight:'700'}}>Profile Screen</Text>
+        <Text>Email:{auth.currentUser.email}</Text>
       </View>
     );
   }
