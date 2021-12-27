684ce88169ea80fc30a35db5775a254a3d5cad93
Yuvaraj2111 <64225422+Yuvaraj2111@users.noreply.github.com>
2021-12-28 00:19:54 +0530

    added bg image

--- screens/sidebar/Signout.js
+++ screens/sidebar/Signout.js
@@ -1,48 +1,49 @@
-import { useNavigation } from '@react-navigation/native'
-import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { auth } from '../../firebase'
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, TouchableOpacity, View,ImageBackground } from "react-native";
+import { auth } from "../../firebase";
 const SignOut = () => {
-    const navigation = useNavigation()
-    const handleSignOut = () => {
-        auth
-            .signOut()
-            .then(() =>{
-                navigation.replace("Login")
-            })
-            .catch(error => alert(error.message))
-    }
-    return (
-        <View style={styles.container}>
-            {/* <Text>Email:{auth.currentUser.email}</Text> */}
-            <TouchableOpacity
-                onPress={handleSignOut} 
-                style={styles.button}>
-                <Text style={styles.buttonText}>Are you sure? Sign Out</Text>
-            </TouchableOpacity>
-        </View>
-    )
-}
+  const navigation = useNavigation();
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login");
+      })
+      .catch((error) => alert(error.message));
+  };
+  return (
+    <ImageBackground 
+    source={require('../../assets/signout.jpg')}
+     style={styles.container}
+     >
+      {/* <Text>Email:{auth.currentUser.email}</Text> */}
+      <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+        <Text style={styles.buttonText}>Are you sure? Sign Out</Text>
+      </TouchableOpacity>
+    </ImageBackground>
+  );
+};
 
-export default SignOut
+export default SignOut;
 
 const styles = StyleSheet.create({
-    container : {
-        flex: 1,
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    button:{
-        backgroundColor:'#28A848',
-        width: '60%',
-        padding: 15,
-        borderRadius:10,
-        alignItems:'center',
-        marginTop:40,
-    },
-    buttonText:{
-         color: 'white',
-         fontWeight:'700',
-         fontSize:16,
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#28A848",
+    width: "60%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 40,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
